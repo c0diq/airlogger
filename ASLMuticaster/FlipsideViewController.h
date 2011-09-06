07896a8c9b7f8d46881daa928cb4c742bdf1172e
Sylvain Rebaud <sylvain@mog.com>
2011-09-06 10:06:33 -0700

    Initial Commit

---
+++ ASLMuticaster/FlipsideViewController.h
@@ -0,0 +1,23 @@
+//
+//  FlipsideViewController.h
+//  ASLMuticaster
+//
+//  Created by Sylvain Rebaud on 9/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FlipsideViewController;
+
+@protocol FlipsideViewControllerDelegate
+- (void)flipsideViewControllerDidFinish:(FlipsideViewController *)controller;
+@end
+
+@interface FlipsideViewController : UIViewController
+
+@property (nonatomic, assign) id <FlipsideViewControllerDelegate> delegate;
+
+- (IBAction)done:(id)sender;
+
+@end
---
+++ ASLMuticaster/MainViewController.h
@@ -0,0 +1,18 @@
+//
+//  MainViewController.h
+//  ASLMuticaster
+//
+//  Created by Sylvain Rebaud on 9/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate> {
+
+}
+
+
+- (IBAction)showInfo:(id)sender;
+
+@end
